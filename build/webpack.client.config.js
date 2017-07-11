4c08bb052949a9e8e79a0ca7253fa370592aefab
swazza <rup@swazza.io>
2017-07-11 17:30:26 +0530

    Updated build config and added dependencies

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -1,15 +1,18 @@
-let commonConfig = require("./webpack.common.config"),
+let common = require("./webpack.common.config"),
   path = require("path"),
   webpack = require("webpack"),
-  HtmlWebpackPlugin = require("html-webpack-plugin");
+  HtmlWebpackPlugin = require("html-webpack-plugin"),
+  UglifyJSPlugin = require("uglifyjs-webpack-plugin"),
+  noop = require("noop-webpack-plugin"),
+  commonConfig = common.commonConfig,
+  getAppConfig = common.getAppConfig;
 
 let devEntries = [
   "react-hot-loader/patch",
-  "webpack-dev-server/client?http://localhost:24000",
+  "webpack-dev-server/client?http://localhost:24000/",
   "webpack/hot/only-dev-server"
 ];
-
-let vendorEntries = [
+let vendorEntries1 = [
   "react",
   "react-dom",
   "react-router",
@@ -21,27 +24,27 @@ let vendorEntries = [
   "redux-saga"
 ];
 
+let vendorEntries2 = ["aphrodite", "graphql-tag", "graphql-anywhere", "react-apollo", "react-motion"];
+
 let appEntries = [path.resolve(__dirname, "../src/client", "index.js")];
 
 module.exports = function(env) {
+  let appConfig = getAppConfig(env);
   let entries = {};
   if (env.dev) {
     entries.dev = devEntries;
   }
 
-  entries.vendor = vendorEntries;
+  entries.vendor1 = vendorEntries1;
+  entries.vendor2 = vendorEntries2;
   entries.app = appEntries;
 
   let clientConfig = {
     target: "web",
-    entry: {
-      dev: env.dev && devEntries,
-      vendor: vendorEntries,
-      app: appEntries
-    },
+    entry: entries,
     output: {
       path: path.resolve(__dirname, "../dist/client"),
-      publicPath: "http://localhost:24000",
+      publicPath: env.dev ? "http://localhost:24000/" : "/public",
       filename: "[name].client.bundle-[hash:6].js"
     },
     devServer: {
@@ -57,14 +60,20 @@ module.exports = function(env) {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        title: "Dash",
-        template: path.resolve(__dirname, "../src/client", "index.html")
+        title: "Book Demo",
+        template: path.resolve(__dirname, "../src/client", "index.html"),
+        minify: {
+          collapseWhitespace: !env.dev,
+          minifyJS: !env.dev
+        }
       }),
-      new webpack.HotModuleReplacementPlugin(),
+      env.dev ? new webpack.HotModuleReplacementPlugin() : noop(),
       new webpack.optimize.CommonsChunkPlugin({
-        name: ["vendor"],
+        name: ["vendor1", "vendor2"],
         minChunks: Infinity
-      })
+      }),
+      new webpack.EnvironmentPlugin(appConfig),
+      !env.dev ? new UglifyJSPlugin() : noop()
     ]
   };
 
--- build/webpack.server.config.js
+++ build/webpack.server.config.js
@@ -1,34 +1,36 @@
-let commonConfig = require('./webpack.common.config'),
-    path = require('path'),
-    fs = require('fs'),
-    webpack = require('webpack');
+let common = require("./webpack.common.config"),
+  path = require("path"),
+  fs = require("fs"),
+  webpack = require("webpack"),
+  commonConfig = common.commonConfig,
+  getAppConfig = common.getAppConfig;
 
 var nodeModules = {};
-fs.readdirSync('node_modules')
-  .filter(function(x) { return ['.bin'].indexOf(x) === -1; })
-  .forEach(function(mod) { nodeModules[mod] = 'commonjs ' + mod; });
+fs
+  .readdirSync("node_modules")
+  .filter(function(x) {
+    return [".bin"].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = "commonjs " + mod;
+  });
 
 module.exports = function(env) {
+  let appConfig = getAppConfig(env);
   let clientConfig = {
-    target: 'node',
+    target: "node",
     entry: {
-      app: path.resolve(__dirname, '../src/server', 'index.js')
+      app: path.resolve(__dirname, "../src/server", "index.js")
     },
     output: {
-      path: path.resolve(__dirname, '../dist/server'),
-      publicPath: '',
-      filename: '[name].server.bundle.js'
+      path: path.resolve(__dirname, "../dist/server"),
+      publicPath: "",
+      filename: "[name].server.bundle.js"
     },
-    devServer: {
-      contentBase: path.join(__dirname, 'dist/client'),
-      port: 24000,
-      historyApiFallback: true,
-      hot: true,
-      host: "0.0.0.0"
-    },
-    externals: nodeModules
+    externals: nodeModules,
+    plugins: [new webpack.EnvironmentPlugin(appConfig)]
   };
 
   let config = Object.assign(commonConfig, clientConfig);
   return config;
-}
+};
